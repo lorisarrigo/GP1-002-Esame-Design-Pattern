317a09baf5321ddbb5d5b305389ebab0c00320be
Loris Arrigo <loris.arrigo@gmail.com>
2026-04-21 12:52:23 +0100

    Inputs
    Implementata InputMap per il Player e modificato lo script di movimento

--- Assets/1 - Scripts/Player Things/PlayerMovement.cs
+++ Assets/1 - Scripts/Player Things/PlayerMovement.cs
@@ -3,18 +3,29 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    InputMap inputs;
+
     [SerializeField] float speed; //Movement Velocity
-    public InputAction Movement; //Input Action that stores the input given in the inspector
     private void Awake()
     {
-        Movement.Enable(); //Enables the Inputs
+        inputs = new InputMap();
+    }
+
+    private void OnEnable()
+    {
+        inputs.Enable();
+    }
+    private void OnDisable()
+    {
+        inputs.Disable();
     }
     private void Update()
     {
-        Vector3 move = Movement.ReadValue<Vector3>(); //sets the movement Input to a Vector3 Value
-
-        //calculate the movement velocity based on the position and the speed of the Player and, olso, the Inputs 
-        Vector3 position = (Vector3)transform.position + Time.deltaTime * speed * move; 
-        transform.position = position; //sets the position
+        Vector3 movementinputs = inputs.Player.Movement.ReadValue<Vector3>();
+        if(movementinputs != Vector3.zero)
+        {
+            Vector3 position = (Vector3)transform.position + Time.deltaTime * speed * movementinputs;
+            transform.position = position;
+        }
     }
 }
